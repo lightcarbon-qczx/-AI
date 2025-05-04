a96fc45d7e2fee8301688e3daa043fed0b2e4595
lightcarbon-qczx <148330074+lightcarbon-qczx@users.noreply.github.com>
2025-05-04 23:36:28 +0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -48,7 +48,7 @@ with st.sidebar:
     """)
     
     # Placeholder image
-    st.image("图片1.jpg", caption="银巢 Logo", use_column_width=True)
+    st.image("图片1.jpg", caption="银巢 Logo", use_container_width=True)
     
     # Contact information
     st.markdown("---")
